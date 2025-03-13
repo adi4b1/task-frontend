921aff7f2625dccce43da294e3a4eba70263e146
adi4b1 <58321312+adi4b1@users.noreply.github.com>
2025-03-13 10:27:24 +0000

    3rd commit "13th code"

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,76 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
+  const [logindata, setlogindata] = useState({
+    email: "",
+    password: "",
+  });
+
+  const loginSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(
+        "https://task-backend-beige.vercel.app/user/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(logindata),
+        }
+      );
+      if (!res.ok) throw new Error("login failed");
+
+      const loginResponse = await res.json();
+      if (res.ok) {
+        localStorage.setItem("token", loginResponse.token);
+        console.log(loginResponse.token);
+        setlogindata({
+          email: "",
+          password: "",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
-        <div>
-            <section>
-                <br />
-                <form className='forLoginDisplay form-control'>
-                 <label htmlFor="">Email</label>
-                    <input type="text"  required={true}/>
-                    <label htmlFor="">Password</label>
-                    <input type="text" required={true}/>
-                    <input type="submit" value='Login'/>
-                </form>
-              
-            </section>
-        </div>
-    </>
-  )
-}
+      <div>
+        <section>
+          <br />
+          <form
+            onSubmit={loginSubmitHandler}
+            className="forLoginDisplay form-control"
+          >
+            <label htmlFor="">Email</label>
+            <input
+              type="email"
+              required={true}
+              name="email"
+              onChange={(e) =>
+                setlogindata({ ...logindata, [e.target.name]: e.target.value })
+              }
+              value={logindata.email}
+            />
 
-export default Login
+            <label htmlFor="">Password</label>
+            <input
+              type="password"
+              required={true}
+              name="password"
+              value={logindata.password}
+              onChange={(e) =>
+                setlogindata({ ...logindata, [e.target.name]: e.target.value })
+              }
+            />
+
+            <input type="submit" value="Login" />
+          </form>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,28 +1,91 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Register = ({showLogin}) => {
+const Register = ({ showLogin }) => {
+  const [data, setdata] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const registerHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        "https://task-backend-beige.vercel.app/user/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Registration failed");
+      }
+      const responseData = await response.json();
+      console.log(responseData);
+
+      setdata({
+        username: "",
+        email: "",
+        password: "",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
-        <div>
-            <br />
-            <section>
-                <form action="" className="forRegisterDisplay form-control">
-                <label htmlFor="">Username</label>
-                <input type="text"  required={true}/>
-                <label htmlFor="">Email</label>
-                <input type="email" required={true}/>
-                <label htmlFor="">Password</label>
-                <input type="password" required={true} />
+      <div>
+        <br />
+        <section>
+          <form
+            onSubmit={registerHandler}
+            className="forRegisterDisplay form-control"
+          >
+            <label htmlFor="">Username</label>
+            <input
+              type="text"
+              name="username"
+              required={true}
+              value={data.username}
+              onChange={(e) =>
+                setdata({ ...data, [e.target.name]: e.target.value })
+              }
+            />
 
-                <input type="submit" value='Register'/>
-                <span>already register?<a onClick={showLogin}>login</a></span>
-                </form>
-               
-              
-            </section>
-        </div>
+            <label htmlFor="">Email</label>
+            <input
+              type="email"
+              name="email"
+              required={true}
+              value={data.email}
+              onChange={(e) =>
+                setdata({ ...data, [e.target.name]: e.target.value })
+              }
+            />
+
+            <label htmlFor="">Password</label>
+            <input
+              type="password"
+              name="password"
+              required={true}
+              value={data.password}
+              onChange={(e) =>
+                setdata({ ...data, [e.target.name]: e.target.value })
+              }
+            />
+
+            <input type="submit" value="Register" />
+            <span>
+              already register?<a onClick={showLogin}>login</a>
+            </span>
+          </form>
+        </section>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
